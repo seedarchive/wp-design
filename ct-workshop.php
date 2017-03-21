a236c9ea5b883438543ed9404bca09c2c5bef4b3
Alan Borning <borning@cs.washington.edu>
2017-03-20 21:20:59 -0700

    hacked in a theme for the SEED workshop

---
+++ ct-workshop.php
@@ -0,0 +1,114 @@
+<?php /* Template Name: ct-workshop-template */ ?>
+<?php define( 'WP_USE_THEMES', false ); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Workshop</title>
+      
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">  
+    
+    <!-- Custom styles for this template -->
+    <link href="/wp-content/themes/seed-design/styles/design.css" rel="stylesheet">
+</head>
+    
+<div id="outer">
+   <div id="container">
+       
+       <?php get_header(); ?>
+       
+       <div id="main-textcontainer">
+            <div id="titlecontainer">
+                <h1>SEED WORKSHOP</h1>
+                <div class="title-underline blue"></div>
+            </div>
+            
+
+<div id="workshopcontainer">
+<h2>Solutions for Economics, Environment and Democracy </h2>
+
+<br/><br/>
+
+<h2>A Workshop at the 
+8th International Conference on Communities &amp; Technologies</h2>
+
+<br/><br/>
+
+<p>
+Organized by W. Lance Bennett and Alan Borning (University of Washington, Seattle USA) 
+and Volker Wulf (University of Siegen, Siegen, Germany)</p>
+
+</div>
+
+<div id="workshopcontainer">
+
+<p>Quality of life for growing numbers of people on the planet is
+threatened by a set of integrated, systemic problems in the economy,
+environment and democracy. The goal of the SEED project is to form an
+international network of scholar/activists, advocates, and practitioners
+who seek to address these problems in a similarly integrated fashion. We
+are approaching this in two ways: first, by developing an online platform
+that connects our participants and facilitates sharing and assessing ideas
+and action experiments operating in local, national and international
+arenas; and second, by organizing a set of in-person workshops and other
+meetings of participants. This Communities &amp; Technologies Workshop will
+be the first of these in-person workshops. Our goals for the workshop are
+three-fold: first, to help shape the plans for the SEED community; second,
+to present and discuss innovative proposals and work in this broad area;
+and third, for a key set of our participants from Europe, North America,
+and elsewhere to meet in person to help establish and further the
+connections among us.  More details are provided in our <a
+href="/articles/seed-workshop-2017-v2.pdf">workshop proposal</a>.</p>
+
+<p>The workshop will take place at the Universit&eacute; de Technologie de
+Troyes, France (<a href="http://comtech.community/venue/">map</a>),
+27 June 2017, from 9:30am-5:00pm, in conjunction with the 
+<a href="http://comtech.community/">Communities &amp; Technologies
+Conference</a>. If you are interested in attending, please send a short
+description of your interests and relevant background by email to
+seedproj@uw.edu, by April 15.  Optionally, you can also include a position
+paper with your application. We will then select a maximum of around 15
+participants, with the goals of including both academics and practitioners,
+and a diverse range of disciplines and skills, in this broad area of
+seeking integrated solutions to problems of economy, environment, and
+democracy.</p>
+
+<p>A central aspect of the SEED project is a <a href="/">resource center</a> of
+position papers and other materials.  We thus also invite attendees to add
+appropriate position papers or other materials to this resource center.  To
+facilitate exchanges at the workshop, we will send out a list of these
+papers in advance.  (Of course, participants can also provide papers later
+to add to the resource center, but these wouldn't be available to the
+participants before the workshop.)</p>
+
+<table>
+<tr><td><b>Important Dates</b></td></tr>
+<tr> <td>Applications due:</td> <td>15 April 2017</td> </tr>
+<tr> <td>Decisions:</td> <td>25 April 2017 (or earlier)</td> </tr>
+<tr> <td>Optional workshop papers due:&nbsp;&nbsp;</td> <td>30 May 2017</td> </tr>
+<tr> <td>Workshop:</td> <td>27 June 2017</td> </tr>
+</table>
+
+<br/>
+<br/>
+
+</div>
+
+
+        <div class="rule black-rule"></div>
+        </div>
+       <?php get_footer(); ?>
+    </div> 
+</div>
+
+
+</html>
