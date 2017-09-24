b0db69883ccfcfa3718c3b7e833670067293ac5d
Alan Borning <borning@cs.washington.edu>
2017-09-23 20:01:37 -0700

    temporarily remove description and links for project mailing lists

--- contact.php
+++ contact.php
@@ -28,7 +28,7 @@
        
        <div id="main-textcontainer">
             <div id="titlecontainer">
-                <h1>CONTACT AND EMAIL LISTS</h1>
+                <h1>CONTACT</h1>
                 <div class="title-underline yellow"></div>
             </div>
             
@@ -43,20 +43,7 @@
                 <a href="http://ccce.com.washington.edu">ccce.com.washington.edu</a>
                 <br/><br/></p>
                 
-                <h3 class="nopadding">Project Mailing Lists<br/><br/></h3>
 
-<p>We invite you to sign up for one or both of our project mailing lists:</p>
-
-<ul>
-
-<li><a href="https://mailman1.u.washington.edu/mailman/listinfo/seed-announce">seed-announce</a>: a low-volume list of project
-announcements (e.g., upcoming workshops, other significant news)</li>
-
-<li><a
-href="https://mailman1.u.washington.edu/mailman/listinfo/seed-discussion">seed-discussion</a>:
-a discussion list for SEED-related topics</li>
-
-</ul>
             </div>
         </div>
        <?php get_footer(); ?>
